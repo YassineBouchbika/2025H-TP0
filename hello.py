e9b1a372b153bc0194414b2b113e56ec6fb6d3ce
YassineBouchbika <yassine.bouchbika@polymtl.ca>
2025-01-14 17:30:05 -0500

    ch.1

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+print(rayon)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+nom = "Bouchbika"
+prénom = "Yassine"
+
+print(nom + " " + prénom)
